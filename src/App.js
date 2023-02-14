df9cc5f62f853f1b1f78631352ebbe4c78a57595
Huy Hùng <huyhung.tdm@gmail.com>
2023-02-14 14:40:18 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div
       className="App"
-      style={{ backgroundColor: "#000", position: "relative", paddingTop: 10 }}
+      style={{ backgroundColor: "#000", position: "relative", paddingTop: 10, minHeight: "100vh" }}
     >
       <ParticleImage
         scale={0.5}
